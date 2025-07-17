7e7be805fb420b2ece8e0537c28c0488b08b53ee
sjzhu0v0 <sjzhu0v0@gmail.com>
2025-07-18 00:54:07 +0200

    Update

--- macro/event_jpsi/MixEventReading.cpp
+++ macro/event_jpsi/MixEventReading.cpp
@@ -36,4 +36,81 @@ void EventMixingReading(TString path_input_flowVecd = "../input.root",
 
   TFile *file_flowVecd = TFile::Open(path_input_flowVecd);
   TFile *fOutput = new TFile(path_output, "RECREATE");
+
+  TTree *tree_input = (TTree *)file_flowVecd->Get("EventMixing");
+
+  ROOT::RDataFrame rdf(*tree_input);
+
+  auto rdf_AllVar =
+      rdf.Define("fPosZ1",
+                 [](const vector<EventData> &events) {
+                   vector<float> vec2return;
+                   for (const auto &event : events) {
+                     vec2return.push_back(event.event_info.fPosZ);
+                   }
+                   return ROOT::VecOps::RVec<float>(vec2return);
+                 },
+                 {"MixedEvent"})
+          .Define("fPosZ2",
+                  [](const vector<EventData> &events) {
+                    vector<float> vec2return;
+                    for (const auto &event : events) {
+                      vec2return.push_back(event.event_info2.fPosZ);
+                    }
+                    return ROOT::VecOps::RVec<float>(vec2return);
+                  },
+                  {"MixedEvent"})
+          .Define("NumContribCalib1",
+                  [](const vector<EventData> &events) {
+                    vector<float> vec2return;
+                    for (const auto &event : events) {
+                      vec2return.push_back(event.event_info.fNumContrib);
+                    }
+                    return ROOT::VecOps::RVec<float>(vec2return);
+                  },
+                  {"MixedEvent"})
+          .Define("NumContribCalib2",
+                  [](const vector<EventData> &events) {
+                    vector<float> vec2return;
+                    for (const auto &event : events) {
+                      vec2return.push_back(event.event_info2.fNumContrib);
+                    }
+                    return ROOT::VecOps::RVec<float>(vec2return);
+                  },
+                  {"MixedEvent"})
+          .Define("vector_Eta_Mass_Pt_Sign_DeltaPhi_DeltaEta",
+                  [](const vector<EventData> &events) {
+                    vector<array<float, 6>> vec2return;
+                    for (const auto &event : events) {
+                      for (size_t i = 0; i < event.jpsi_info.fPT.size(); ++i) {
+                        for (size_t j = i + 1; j < event.jpsi_info.fPT.size();
+                             ++j) {
+                          if (event.jpsi_info.fSign[i] == 0 &&
+                              event.jpsi_info.fSign[j] == 0) {
+                            float deltaPhi = event.jpsi_info.fPhi[i] -
+                                             event.jpsi_info.fPhi[j];
+                            if (deltaPhi > M_PI)
+                              deltaPhi -= 2 * M_PI;
+                            else if (deltaPhi < -M_PI)
+                              deltaPhi += 2 * M_PI;
+                            float deltaEta = event.jpsi_info.fEta[i] -
+                                             event.jpsi_info.fEta[j];
+                            vec2return.push_back({event.jpsi_info.fEta[i],
+                                                  event.jpsi_info.fMass[i],
+                                                  event.jpsi_info.fPT[i],
+                                                  event.jpsi_info.fSign[i],
+                                                  deltaPhi, deltaEta});
+                          }
+                        }
+                      }
+                    }
+                  },
+                  {"MixedEvent"});
+}
+
+int main(int argc, char **argv) {
+  TString path_input_flowVecd = argv[1];
+  TString path_output = argv[2];
+  EventMixingReading(path_input_flowVecd, path_output);
+  return 0;
 }
