056b24f489c7ee32145e67cdac609f1990cf6c67
sjzhu0v0 <sjzhu0v0@gmail.com>
2025-08-14 16:58:41 +0200

    Update

--- macro/event_jpsi/JpsiAsso.cpp
+++ macro/event_jpsi/JpsiAsso.cpp
@@ -60,11 +60,11 @@ void JpsiAsso(
           .Filter("isntSameBunchPileup", "no Time Frame border")
       /*  .Filter("isntSelfDefinedPileup", "no self defined pileup") */;
 
-  auto CutTrackInfo = [](const TrackInfo &track_info, const int &index) {
-    bool ptCut =
-        track_info.fPTREF[index] > 0.4 && track_info.fPTREF[index] < 4.0;
-    return ptCut;
-  };
+  // auto CutTrackInfo = [](const TrackInfo &track_info, const int &index) {
+  //   bool ptCut =
+  //       track_info.fPTREF[index] > 0.4 && track_info.fPTREF[index] < 4.0;
+  //   return ptCut;
+  // };
 
   auto rdf_PartTriggerWithJpsiWithEvent =
       rdf_PartTrigger
@@ -75,14 +75,16 @@ void JpsiAsso(
                    "fMass", "fSign", "fPTREF", "fEtaREF", "fPhiREF"})
           .Define(
               "JpsiInfoUS",
-              [CutTrackInfo](const EventData &eventData) {
+              [](const EventData &eventData) {
                 ROOT::VecOps::RVec<array<float, 6>> vec2return;
                 for (size_t i = 0; i < eventData.jpsi_info.fPT.size(); ++i) {
                   if (eventData.jpsi_info.fSign[i] == 0) {
                     for (size_t j = 0; j < eventData.track_info.fEtaREF.size();
                          ++j) {
-                      if (!CutTrackInfo(eventData.track_info, j))
-                        continue;
+                      cout << "TrackInfo size: "
+                           << eventData.track_info.fEtaREF.size() << endl;
+                      // if (!CutTrackInfo(eventData.track_info, j))
+                      //   continue;
                       float delta_eta = eventData.jpsi_info.fEta[i] -
                                         eventData.track_info.fEtaREF[j];
                       float delta_phi = eventData.jpsi_info.fPhi[i] -
@@ -203,7 +205,7 @@ void JpsiAsso(
 
   // MHnTool hnTool(gRResultHandles[0].GetPtr<THnD>());
   // hnTool.PrintAllAxis();
-cout << "Start writing THnD objects..." << endl;
+
   fOutput->cd();
   RResultWrite(gRResultHandles);
   fOutput->Close();
