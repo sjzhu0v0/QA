63ca2c4dc1dc91221285af295f6ec6dae9cf4fd1
sjzhu0v0 <sjzhu0v0@gmail.com>
2025-09-01 19:21:26 +0200

    Update

--- macro/event_jpsi/NJpsiCandidatePerEvent.cpp
+++ macro/event_jpsi/NJpsiCandidatePerEvent.cpp
@@ -81,6 +81,16 @@ void NJpsiCandidatePerEvent(
                         double deltaY = p1.Rapidity() - p2.Rapidity();
                         pairs.push_back(
                             {mass[i], mass[j], pt[i], pt[j], deltaY});
+                      } else {
+                        cout << "info1_1:" << sign1[i] << " " << sign2[i] << " "
+                             << pt1[i] << " " << eta1[i] << " " << phi1[i]
+                             << " " << pt2[i] << " " << eta2[i] << " "
+                             << phi2[i] << endl;
+                        cout << "info1_2:" << sign1[j] << " " << sign2[j] << " "
+                             << pt1[j] << " " << eta1[j] << " " << phi1[j]
+                             << " " << pt2[j] << " " << eta2[j] << " "
+                             << phi2[j] << endl;
+                        cout << endl;
                       }
                     }
                   }
