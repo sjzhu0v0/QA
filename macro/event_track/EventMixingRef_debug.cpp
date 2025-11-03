fa7514150de53a229ed9646cab75e5b133f911ca
sjzhu0v0 <sjzhu0v0@gmail.com>
2025-11-03 11:28:22 +0100

    Update

--- macro/event_track/EventMixingRef_debug.cpp
+++ macro/event_track/EventMixingRef_debug.cpp
@@ -1,7 +1,8 @@
 #define MRDF
 #include <ROOT/RDataFrame.hxx>
-
-#include "iostream"
+#include <TFile.h>
+#include <TH1.h>
+#include <iostream>
 
 using namespace std;
 
@@ -9,49 +10,26 @@ void EventMixingRef(TString path_input_flowVecd = "../input.root",
                     TString path_input_mult = "../input2.root",
                     TString path_output = "output.root",
                     TString path_output_tree = "output_tree.root") {
-  // close multi-thread
+  // Disable multi-threading
   ROOT::EnableImplicitMT(1);
 
-  // TFile *file_flowVecd = TFile::Open(path_input_flowVecd);
-  // TFile *file_mult = TFile::Open(path_input_mult);
+  cout << "Input file (flowVecd): " << path_input_flowVecd << endl;
+  cout << "Input file (mult):     " << path_input_mult << endl;
+  cout << "Output file:           " << path_output << endl;
+  cout << "Output tree file:      " << path_output_tree << endl;
 
-  // Calib_NumContrib_fPosZ_Run::GetHistCali(path_calib, runNumber);
-  // Cut_MultTPC_NumContrib::init(path_pileup);
+  // Use RDataFrame directly from file path (recommended)
+  ROOT::RDataFrame rdf_mult("MultCalib", path_input_mult.Data());
 
-  // TChain *tree_flowVecd = MRootIO::OpenChain(file_flowVecd, "O2dqflowvecd");
-  // TChain *tree_mult = MRootIO::OpenChain(file_mult, "MultCalib");
+  // Create histogram
+  auto hist_mult = rdf_mult.Histo1D({"NumContribCalib_hist", "NumContribCalib;N_{contrib};Entries", 1000, 0, 10000}, "NumContribCalib");
 
-  cout << "Input file: " << path_input_flowVecd << endl;
-  cout << "Input file: " << path_input_mult << endl;
-  cout << "Output file: " << path_output << endl;
-  cout << "Output tree file: " << path_output_tree << endl;
-  // TChain *tree_mult = MRootIO::OpenChain(path_input_mult, "MultCalib");
-  // TTree *tree_mult = GetObjectSingle<TTree>(
-  //     "/lustre/alice/users/szhu/job/QA/LHC24pass1_DiElectron_Group/JpsiQA_mult/"
-  //     "JpsiQA_mult_554207.root :MultCalib");
-  TFile *file_mult =
-      TFile::Open("/lustre/alice/users/szhu/job/QA/LHC24pass1_DiElectron_Group/"
-                  "JpsiQA_mult/JpsiQA_mult_554207.root");
-  TTree *tree_mult = (TTree *)file_mult->Get("MultCalib");
+  // Save to output file
+  TFile outFile(path_output, "RECREATE");
+  hist_mult->Write();
+  outFile.Close();
 
-  // tree_flowVecd->AddFriend(tree_mult);
-
-  tree_mult->Print();
-
-  ROOT::RDataFrame rdf_flowVecd(*tree_mult);
-  ROOT::RDataFrame rdf_mult(
-      "MultCalib",
-      "/lustre/alice/users/szhu/job/QA/LHC24pass1_DiElectron_Group/"
-      "JpsiQA_mult/JpsiQA_mult_558275.root");
-  // auto report = rdf_flowVecd.Report();
-  // report->Print();
-
-  // auto rdf_witTrigger =
-  //     rdf_flowVecd.Define("map_trigger", MALICE::triggermapRVec,
-  //     {"fSelection"});
-
-  auto hist_mult = rdf_mult.Histo1D("NumContribCalib");
-  hist_mult->Draw();
+  cout << "Histogram saved to " << path_output << endl;
 }
 
 int main(int argc, char **argv) {
@@ -60,22 +38,14 @@ int main(int argc, char **argv) {
   TString path_output = "output.root";
   TString path_output_tree = "output_tree.root";
 
-  if (argc > 1) {
-    path_input_flowVecd = argv[1];
-  }
-  if (argc > 2) {
-    path_input_mult = argv[2];
-  }
-  if (argc > 3) {
-    path_output = argv[3];
-  }
-  if (argc > 4) {
-    path_output_tree = argv[4];
-  }
+  if (argc > 1) path_input_flowVecd = argv[1];
+  if (argc > 2) path_input_mult     = argv[2];
+  if (argc > 3) path_output         = argv[3];
+  if (argc > 4) path_output_tree    = argv[4];
 
-  gROOT->SetBatch(kTRUE); // Disable interactive graphics
-  EventMixingRef(path_input_flowVecd, path_input_mult, path_output,
-                 path_output_tree);
+  gROOT->SetBatch(kTRUE); // No graphics
+
+  EventMixingRef(path_input_flowVecd, path_input_mult, path_output, path_output_tree);
 
   return 0;
 }
