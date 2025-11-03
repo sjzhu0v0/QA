ebc3f2e189942311ed8671343cbc09ff1da40882
sjzhu0v0 <sjzhu0v0@gmail.com>
2025-11-03 11:05:03 +0100

    Update

--- macro/event_track/EventMixingRef_debug.cpp
+++ macro/event_track/EventMixingRef_debug.cpp
@@ -80,10 +80,10 @@ void EventMixingRef(TString path_input_flowVecd = "../input.root",
   auto report = rdf.Report();
   report->Print();
 
-  // auto rdf_witTrigger =
-  //     rdf.Define("map_trigger", MALICE::triggermapRVec, {"fSelection"});
+  auto rdf_witTrigger =
+      rdf.Define("map_trigger", MALICE::triggermapRVec, {"fSelection"});
 
-  // rdf_witTrigger.Histo1D("map_trigger")->Draw();
+  rdf_witTrigger.Histo1D("map_trigger")->Draw();
 }
 
 int main(int argc, char **argv) {
