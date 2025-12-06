b327529d640bb5ef3e586f38c070401f199b9b27
sjzhu0v0 <sjzhu0v0@gmail.com>
2025-12-06 15:15:10 +0100

    Update

--- macro/track/Efficiency.cpp
+++ macro/track/Efficiency.cpp
@@ -279,10 +279,11 @@ void Efficiency(TString path_input, TString path_output) {
       var_mult.fName.Data()));
 
   gRResultHandles.push_back(rdf_mc_selected_kine.Histo1D(
-      GetTH1DM(var_mult, "mc_mult", "mc_mult, |#eta|<0.9"),
+      GetTH1DM(var_mult, "mc_mult", "mc_mult, |#eta|<0.9,1GeV/c<p_{T}<5GeV/c"),
       var_mult.fName.Data()));
   gRResultHandles.push_back(rdf_reco_selected_kine.Histo1D(
-      GetTH1DM(var_mult, "reco_mult", "reco_mult, |#eta|<0.9"),
+      GetTH1DM(var_mult, "reco_mult",
+               "reco_mult, |#eta|<0.9,1GeV/c<p_{T}<5GeV/c"),
       var_mult.fName.Data()));
 
   RunGraphs(gRResultHandles);
