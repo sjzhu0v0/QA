30c75a36a33ff2c99223839fc629668d982b263c
sjzhu0v0 <sjzhu0v0@gmail.com>
2025-12-23 22:55:00 +0100

    Update

--- macro/event_jpsi/EventMixingJpsiAssoPair.cpp
+++ macro/event_jpsi/EventMixingJpsiAssoPair.cpp
@@ -300,8 +300,6 @@ void EventMixingJpsiAssoPair(TString path_input_flowVecd = "../input1.root",
 
   long long iEntry = -1;
   while (rPairs.Next()) {
-    rPairs.SetEntry(iEntry);
-
     for (const auto& abPair_single : *abPair) {
       ULong64_t entryA = abPair_single.first;
       ULong64_t entryB = abPair_single.second;
